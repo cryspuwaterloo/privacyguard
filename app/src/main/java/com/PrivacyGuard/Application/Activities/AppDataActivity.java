fbd70c6abedaf29fc2dc9a8a1ee505d76b03492d
lucaspalmer8 <lucaspalmer8@gmail.com>
2017-02-17 01:09:57 -0500

    Adding correct permission and starting to get usage statistics.

--- app/src/main/java/com/PrivacyGuard/Application/Activities/AppDataActivity.java
+++ app/src/main/java/com/PrivacyGuard/Application/Activities/AppDataActivity.java
@@ -1,9 +1,18 @@
 package com.PrivacyGuard.Application.Activities;
 
+import android.annotation.TargetApi;
+import android.app.usage.UsageEvents;
+import android.app.usage.UsageStatsManager;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by lucas on 16/02/17.
@@ -14,7 +23,10 @@ public class AppDataActivity extends AppCompatActivity {
     public static final String APP_NAME_INTENT = "APP_NAME";
     public static final String APP_PACKAGE_INTENT = "PACKAGE_NAME";
 
+    UsageStatsManager usageStatsManager;
+
     @Override
+    @TargetApi(22)
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -24,6 +36,28 @@ public class AppDataActivity extends AppCompatActivity {
         String appName = i.getStringExtra(APP_NAME_INTENT);
         String packageName = i.getStringExtra(APP_PACKAGE_INTENT);
 
-        Toast.makeText(this, appName + packageName, Toast.LENGTH_LONG).show();
+        if (android.os.Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP_MR1){
+            Toast.makeText(this, "Sorry, this feature is not supported in your version of Android", Toast.LENGTH_LONG).show();
+        }
+
+        usageStatsManager = (UsageStatsManager) getSystemService(USAGE_STATS_SERVICE);
+
+        long time = System.currentTimeMillis();
+        System.out.println(TimeUnit.DAYS.toMillis(30));
+        UsageEvents usageEvents = usageStatsManager.queryEvents(time - TimeUnit.DAYS.toMillis(30), time);
+
+        List<UsageEvents.Event> myUsageEvents = new ArrayList<>();
+        while (usageEvents.hasNextEvent()) {
+            UsageEvents.Event event = new UsageEvents.Event();
+            usageEvents.getNextEvent(event);
+            System.out.println(event.getPackageName() + "    ~~~!!!!!00");
+            if (event.getPackageName().equals(packageName)) {
+                myUsageEvents.add(event);
+            }
+        }
+
+        Toast.makeText(this, "" + myUsageEvents.size(), Toast.LENGTH_LONG).show();
+
+        startActivityForResult(new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS), 0);
     }
 }
