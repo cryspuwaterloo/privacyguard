b781d077d17824dacc95177058aae78a6664bca9
lucaspalmer8 <lucaspalmer8@gmail.com>
2017-03-08 23:40:51 -0500

    Working version of Android Plot with Privacy Guard

--- app/src/main/java/com/PrivacyGuard/Application/Activities/AppDataActivity.java
+++ app/src/main/java/com/PrivacyGuard/Application/Activities/AppDataActivity.java
@@ -9,25 +9,22 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
-import android.support.v4.util.TimeUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.PrivacyGuard.Application.Database.DataLeak;
 import com.PrivacyGuard.Application.Database.DatabaseHandler;
 import com.PrivacyGuard.Plugin.LeakReport;
-import com.androidplot.ui.Size;
-import com.androidplot.ui.SizeMode;
 import com.androidplot.xy.BoundaryMode;
 import com.androidplot.xy.LineAndPointFormatter;
 import com.androidplot.xy.SimpleXYSeries;
 import com.androidplot.xy.StepMode;
 import com.androidplot.xy.XYGraphWidget;
 import com.androidplot.xy.XYPlot;
-import com.androidplot.xy.XYSeries;
 
 import java.text.DateFormat;
 import java.text.FieldPosition;
@@ -61,6 +58,8 @@ public class AppDataActivity extends AppCompatActivity {
     private View invalidAndroidVersionView;
     private View permissionDisabledView;
     private RelativeLayout contentView;
+    private ImageButton navigateLeft;
+    private ImageButton navigateRight;
 
     private UsageStatsManager usageStatsManager;
     private DatabaseHandler databaseHandler;
@@ -94,6 +93,23 @@ public class AppDataActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS));
+            }
+        });
+
+        navigateLeft = (ImageButton)findViewById(R.id.navigate_left);
+        navigateRight = (ImageButton)findViewById(R.id.navigate_right);
+        navigateLeft.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                currentListIndex = Math.max(currentListIndex - 1, 0);
+                setUpGraph();
+            }
+        });
+        navigateRight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                currentListIndex = Math.min(currentListIndex + 1, leaksList.size() - 1);
+                setUpGraph();
             }
         });
     }
@@ -151,6 +167,13 @@ public class AppDataActivity extends AppCompatActivity {
     }
 
     private void setUpGraph() {
+        boolean navigateRightEnabled = currentListIndex < leaksList.size() - 1;
+        boolean navigateLeftEnabled = currentListIndex > 0;
+        navigateRight.setEnabled(navigateRightEnabled);
+        navigateRight.setAlpha(navigateRightEnabled ? 1.0f : 0.3f);
+        navigateLeft.setEnabled(navigateLeftEnabled);
+        navigateLeft.setAlpha(navigateLeftEnabled ? 1.0f : 0.3f);
+
         XYPlot plot = (XYPlot) findViewById(R.id.plot);
         plot.clear();
 
@@ -211,7 +234,7 @@ public class AppDataActivity extends AppCompatActivity {
         }
 
         for (int i = 0; i < leakCategories.length; i++) {
-            SimpleXYSeries series = new SimpleXYSeries(leakCategories[i].name());
+            SimpleXYSeries series = new SimpleXYSeries(leakCategories[i].name().substring(0, 1));
             Map<String, Integer> leaks = leakMaps.get(i);
             for(String timeStamp : leaks.keySet()) {
                 series.addLast(databaseHandler.getDateFromTimestamp(timeStamp).getTime(), leaks.get(timeStamp));
@@ -221,17 +244,21 @@ public class AppDataActivity extends AppCompatActivity {
 
         rangeUpperBound++;
         plot.setRangeBoundaries(0, rangeUpperBound + rangeUpperBound % 2, BoundaryMode.FIXED);
-        plot.setRangeStep(StepMode.INCREMENT_BY_VAL, 2);
+        plot.setRangeStep(StepMode.INCREMENT_BY_VAL, 1);
         plot.setDomainStep(StepMode.INCREMENT_BY_VAL, 2000);
         DateFormat dateFormat = new SimpleDateFormat("MMMM dd, yyyy", Locale.CANADA);
         plot.setTitle(dateFormat.format(new Date(domainUpperBound)));
         plot.getGraph().getLineLabelStyle(XYGraphWidget.Edge.BOTTOM).setFormat(new GraphDomainFormat());
         plot.getGraph().getLineLabelStyle(XYGraphWidget.Edge.LEFT).setFormat(new GraphRangeFormat());
-        plot.getLegend().setSize(new Size(200, SizeMode.ABSOLUTE, 400, SizeMode.ABSOLUTE));
+        plot.getGraph().getDomainCursorPaint().setTextSize(10);
+        plot.getGraph().setPaddingLeft(50);
+        plot.getGraph().setPaddingRight(50);
+        plot.getGraph().setPaddingBottom(200);
+        plot.redraw();
     }
 
     private static class GraphDomainFormat extends Format {
-        private static DateFormat dateFormat = new SimpleDateFormat("hh:mm:ss aa", Locale.CANADA);
+        private static DateFormat dateFormat = new SimpleDateFormat("h:mm:ss aa", Locale.CANADA);
 
         @Override
         public StringBuffer format(Object obj, StringBuffer toAppendTo, FieldPosition pos) {
@@ -249,7 +276,7 @@ public class AppDataActivity extends AppCompatActivity {
         @Override
         public StringBuffer format(Object obj, StringBuffer toAppendTo, FieldPosition pos) {
             int quantity = ((Number) obj).intValue();
-            return toAppendTo.append(quantity);
+            return toAppendTo.append(quantity % 2 == 0 ? quantity : "");
         }
         @Override
         public Object parseObject(String source, ParsePosition pos) {
